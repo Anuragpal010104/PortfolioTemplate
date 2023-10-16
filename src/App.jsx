275276b7a4de476feb4f5c2165faae824d98906f
Anurag Pal <120403013+Anuragpal010104@users.noreply.github.com>
2023-10-16 16:26:34 +0000

    Changed the details about me

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Anurag Pal",
+  title: "Web Developer",
+  email: "anuragpal2004@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
